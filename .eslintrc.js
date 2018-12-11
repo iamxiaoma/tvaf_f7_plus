e37f1b725c20a4626a409819ebf64702d9b244f4
iamxiaoma <395487290@qq.com>
2018-12-11 16:58:27 +0800

    更新依赖

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,8 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'eslint-disable-next-line': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {
     parser: 'babel-eslint'
